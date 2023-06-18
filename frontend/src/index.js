0ca560b937dbbca4b4528a8d0cbea63b335a12b6
bakshiyogesh <yogeshbakshi589@gmail.com>
2023-06-18 07:38:36 +0000

    theme changed for styled components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,20 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import React from "react";
 import { store } from "./store";
-import {theme} from "./styles/theme";
+import theme from "./styles/theme";
 import {ThemeProvider} from 'styled-components';
 
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
-
+const div=styled.div`dark: "#170539";`;
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter basename="/">
+      <div>
       <ThemeProvider theme={theme}>
       <App />
       </ThemeProvider>
+      </div>
     </BrowserRouter>
   </Provider>,
 
