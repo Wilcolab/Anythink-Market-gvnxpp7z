27472200abdaf1136646474ba42e8358be66fa05
bakshiyogesh <yogeshbakshi589@gmail.com>
2023-06-18 07:09:49 +0000

    "theme provider added."

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import React from "react";
 import { store } from "./store";
+import {theme} from "./styles/theme";
+import {ThemeProvider} from 'styled-components';
 
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
@@ -10,7 +12,9 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter basename="/">
+      <ThemeProvider theme={theme}>
       <App />
+      </ThemeProvider>
     </BrowserRouter>
   </Provider>,
 
