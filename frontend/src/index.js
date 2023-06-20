f498fc85622dd74a7754afcfa1fdf4c298e32479
bakshiyogesh <yogeshbakshi589@gmail.com>
2023-06-20 15:44:13 +0000

    trying to add finally.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,13 +15,13 @@ background-color:"#170539",
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter basename="/">
-    <StyledBanner props={{theme}}>
       <Content>
+    <StyledBanner props={{theme}}>
       <ThemeProvider theme={theme}>
       <App />
       </ThemeProvider>
-      </Content>
       </StyledBanner>
+      </Content>
     </BrowserRouter>
   </Provider>,
 
